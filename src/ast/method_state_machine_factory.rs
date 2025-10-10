e9bfb0e7574b165c3d7319ef564678abf64993a2
Stu Forbes <stu@stuforbes.com>
2025-10-10 13:13:27 +0100

    Initial state definitions for method builder.
    Parking this approach for now, as it feels like it will be very verbose

--- src/ast/method_state_machine_factory.rs
+++ src/ast/method_state_machine_factory.rs
@@ -1,15 +1,31 @@
 use crate::ast::state_machine::{Operation, StateMachine};
 use crate::scanner::TokenType;
 use std::collections::HashMap;
+use crate::ast::class_state_machine_factory::ClassState::MethodStatic;
 
 #[derive(Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Copy, Clone)]
 pub enum MethodState {
     Initial,
-    ClassScope,
+    Identifier,
+    IdentifierFullyQualifiedSeparator,
+
+    MethodInvocationParameters,
+    MethodInvocationParameterIdentifier,
+    MethodInvocationParameterIdentifierFullyQualifiedSeparator,
+
 }
 
 pub fn load() -> StateMachine<MethodState> {
-    let state_contexts: HashMap<(MethodState, TokenType), Operation<MethodState>> = vec![]
+    let state_contexts: HashMap<(MethodState, TokenType), Operation<MethodState>> = vec![
+        ((MethodState::Initial, TokenType::Identifier), Operation::To(MethodState::Identifier)),
+        ((MethodState::Identifier, TokenType::Dot), Operation::To(MethodState::IdentifierFullyQualifiedSeparator)),
+        ((MethodState::IdentifierFullyQualifiedSeparator, TokenType::Identifier), Operation::To(MethodState::Identifier)),
+        ((MethodState::Identifier, TokenType::LeftParen), Operation::To(MethodState::MethodInvocationParameters)),
+
+        ((MethodState::MethodInvocationParameters, TokenType::Identifier), Operation::To(MethodState::MethodInvocationParameterIdentifier)),
+        ((MethodState::MethodInvocationParameterIdentifier, TokenType::Dot), Operation::To(MethodState::MethodInvocationParameterIdentifierFullyQualifiedSeparator)),
+
+    ]
         .into_iter()
         .collect();
 
