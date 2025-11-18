b60cef5e71606e81f54c24cccad08980039f17bb
Stu Forbes <stu@stuforbes.com>
2025-11-18 20:06:59 +0000

    Fleshing out variable assignment instruction handler

--- src/compiler/instruction/variable_assignment.rs
+++ src/compiler/instruction/variable_assignment.rs
@@ -1,7 +1,16 @@
 use ristretto_classfile::attributes::Instruction;
 use crate::ast::expression::Expression;
-use crate::compiler::CompileResult;
+use crate::compiler::{CompilationContext, CompileResult};
+use crate::compiler::instruction::expression::from_expression;
 
-pub fn from_variable_assignment(_name: &str, _var_type: Option<&str>, _is_final: bool, _value: &Option<Expression>) -> CompileResult<Vec<Instruction>> {
-    panic!("Not implemented")
+pub fn from_variable_assignment(_name: &str, _var_type: Option<&str>, _is_final: bool, value: &Option<Expression>, compilation_context: &mut CompilationContext) -> CompileResult<Vec<Instruction>> {
+
+    if value.is_none() {
+        panic!("Unable to handle uninitialised variables");
+    }
+
+    let mut result= from_expression(&value.as_ref().unwrap(), compilation_context)?;
+    result.push(Instruction::Astore(1));
+
+    Ok(result)
 }
